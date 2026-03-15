956ae25a5f3401e881ab186636562c26e80d136e
Devon-Robinson0 <devon-robinson2@live.nmit.ac.nz>
2026-03-16 10:41:22 +1300

    completed card counter

---
+++ Functions/card-counter.js
@@ -0,0 +1,22 @@
+let count = 0;
+
+function cardCounter(card) {
+    if (card >= 2 && card <= 6) {
+      count++;
+    } else if (card === 10 || card === "J" || card === "Q" || card === "K" || card === "A") {
+      count--;
+    }
+  
+  if (count > 0) {
+    return `${count} Bet`;
+  } else {
+    return `${count} Hold`;
+  }
+
+}
+
+console.log(cardCounter(10));
+console.log(cardCounter("J"));
+console.log(cardCounter("Q"));
+console.log(cardCounter("K"));
+console.log(cardCounter("A"));
